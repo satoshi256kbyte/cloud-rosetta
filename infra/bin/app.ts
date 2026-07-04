99eadd7c95903b6fa859406dc6a08350b8b23392
Satoshi Kaneyasu <satoshi.kaneyasu@serverworks.co.jp>
2026-07-04 18:30:43 +0900

    refactor(infra): Stage廃止でcdk deploy --allを正常動作させる

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -1,19 +1,26 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { AwsSolutionsChecks } from 'cdk-nag';
-import { DevStage } from '../lib/stages/dev';
+import { StorageStack } from '../lib/stacks/storage-stack';
+import { applyTags } from '../lib/utils/tagging';
 
 const app = new cdk.App();
 
 // cdk-nag AwsSolutions パックを全スタックに適用
 cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
 
-// dev 環境
-new DevStage(app, 'Dev', {
+const stage = 'dev';
+
+// Storage Stack
+new StorageStack(app, 'StorageStack', {
+  stage,
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: 'ap-northeast-1',
   },
 });
+
+// 必須タグを全リソースに付与
+applyTags(app, stage);
 
 app.synth();
